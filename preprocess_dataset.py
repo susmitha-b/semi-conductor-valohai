16ccc10734cf62ddb66f764a0292b765af4e1200
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-18 15:34:13 +0530

    Update preprocess_dataset.py

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -20,44 +20,44 @@ def main():
     # and cache the data for quick experimentation
 
     print('Loading data')
-    with pd.read_csv(valohai.inputs('dataset').path()) as data:
-        check = data.isnull().any().any()
-        if check:
-           data = data.replace(np.NaN, 0)
-        def remove_collinear_features(x, threshold):
-            corr_matrix = x.corr()
-            iters = range(len(corr_matrix.columns) - 1)
-            drop_cols = []
+    data = pd.read_csv(valohai.inputs('dataset').path())
+    check = data.isnull().any().any()
+    if check:
+       data = data.replace(np.NaN, 0)
+    def remove_collinear_features(x, threshold):
+        corr_matrix = x.corr()
+        iters = range(len(corr_matrix.columns) - 1)
+        drop_cols = []
 
-            # Iterate through the correlation matrix and compare correlations
-            for i in iters:
-                for j in range(i+1):
-                    item = corr_matrix.iloc[j:(j+1), (i+1):(i+2)]
-                    col = item.columns
-                    row = item.index
-                    val = abs(item.values)
+        # Iterate through the correlation matrix and compare correlations
+        for i in iters:
+            for j in range(i+1):
+                item = corr_matrix.iloc[j:(j+1), (i+1):(i+2)]
+                col = item.columns
+                row = item.index
+                val = abs(item.values)
 
-                    # If correlation exceeds the threshold
-                    if val >= threshold:
-                       # Print the correlated features and the correlation value
-                       print(col.values[0], "|", row.values[0], "|", round(val[0][0], 2))
-                       drop_cols.append(col.values[0])
+                # If correlation exceeds the threshold
+                if val >= threshold:
+                # Print the correlated features and the correlation value
+                   print(col.values[0], "|", row.values[0], "|", round(val[0][0], 2))
+                   drop_cols.append(col.values[0])
 
             # Drop one of each pair of correlated columns
-            drops = set(drop_cols)
-            x = x.drop(columns=drops)
+         drops = set(drop_cols)
+         x = x.drop(columns=drops)
 
-            return x       
+         return x       
             
-        data = remove_collinear_features(data,0.70)
+    data = remove_collinear_features(data,0.70)
         
-        # separating the dependent and independent data
-        x = data.iloc[:,:306]
-        y = data["Pass_Fail"]
+    # separating the dependent and independent data
+    x = data.iloc[:,:306]
+    y = data["Pass_Fail"]
         
-        print('Preprocessing data')
-        #x_train, x_test = x_train / 255.0, x_test / 255.0
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 1)
+    print('Preprocessing data')
+    #x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 1)
         
     # Write output files to Valohai outputs directory
     # This enables Valohai to version your data
