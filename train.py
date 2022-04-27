b06e096b74ebe7459582d850965f473990612b9b
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-27 11:10:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,8 @@
 import uuid
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from xgboost.sklearn import XGBClassifier
 import valohai
 
 def main():
@@ -14,10 +11,10 @@ def main():
         step='train-model',
         image='valohai/sklearn:1.0',
         default_inputs={
-          'dataset1': 'datum://018065d6-cd28-5216-6271-aa7c8d892ecc',
-          'dataset2': 'datum://018065d6-d133-ac3b-9db9-b468c7bf9e34',
-          'dataset3': 'datum://018065d6-cf87-eff3-b24a-e692039ad468',
-          'dataset4': 'datum://018065d6-d2d4-f51f-4916-29520631b971',
+          'dataset1': 'datum://01806971-5724-09f4-2604-4ca1dda335b6',
+          'dataset2': 'datum://01806971-58b4-8257-dffe-3a2b02aef9c7',
+          'dataset3': 'datum://01806971-5a3f-726d-03da-15920ab9cade',
+          'dataset4': 'datum://01806971-5bca-2dda-158b-15d70de54d9f',
         },
     )
   
@@ -26,27 +23,13 @@ def main():
   x_test = pd.read_csv(valohai.inputs('dataset3').path())
   y_test = pd.read_csv(valohai.inputs('dataset4').path())
   
-  #y_train_repl=y_train.replace(-1,0)
-  #y_test_repl=y_test.replace(-1,0)
-  #clf = XGBClassifier(max_depth=6,n_estimators=100)
-  #clf.fit(x_train, y_train_repl)
-  #y_pred1 = clf.predict(x_test)
-  #test_accuracy_xgb = clf.score(x_test,y_test_repl)*100
-  
   rf = RandomForestClassifier(n_estimators=100, random_state=1,verbose=0)
   rf.fit(x_train, y_train)
   y_pred2 = rf.predict(x_test)
   test_accuracy_rf = rf.score(x_test,y_test)*100
   
-  lr = LogisticRegression(random_state=1)
-  lr.fit(x_train, y_train) 
-  y_pred3 = lr.predict(x_test)
-  test_accuracy_lr = lr.score(x_test,y_test)*100
-  
   with valohai.logger() as logger:
-      #logger.log('test_accuracy_xgb', test_accuracy_xgb)
       logger.log('test_accuracy_rf', test_accuracy_rf)
-      logger.log('test_accuracy_lr', test_accuracy_lr)
       
   # printing the confusion matrix
   #cm = confusion_matrix(y_test_os, y_pred)
@@ -55,13 +38,8 @@ def main():
   #cm = confusion_matrix(y_test_os, y_pred)
   #sns.heatmap(cm, annot = True, cmap = 'rainbow')
   suffix = uuid.uuid4()
-  #output_path1 = valohai.outputs().path('model_xgb.h5')
-  output_path2 = valohai.outputs().path('model_rf.h5')
-  output_path3 = valohai.outputs().path('model_lr.h5')
-  #xgb.save(output_path1)
-  rf.save(output_path2)
-  lr.save(output_path3)
-
+  output_path = valohai.outputs().path('model_rf.h5')
+  rf.save(output_path)
 
 if __name__ == '__main__':
     main()
